2313ab01d7f51c7f93f88148367a3fab03a8e240
Kenny <kennysalazar3197@gmail.com>
2022-05-31 21:07:01 -0500

    Create Posts controller and .../posts route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
 end
