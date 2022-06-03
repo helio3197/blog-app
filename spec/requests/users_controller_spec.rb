5f71aa69608649203fc0ecd2827e1060f52ece92
Kenny <kennysalazar3197@gmail.com>
2022-06-02 20:47:30 -0500

    Add tests to users#show

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Users controller tests' do
-  context 'Users#index' do
+  context 'users#index' do
     before(:example) { get '/users' }
 
     it 'Response status should be 200: ok' do
@@ -14,6 +14,22 @@ describe 'Users controller tests' do
 
     it 'The response body should contain placeholder text' do
       expect(response.body).to include('User name')
+    end
+  end
+
+  context 'users#show' do
+    before(:example) { get '/users/1' }
+
+    it 'Response status should be 200: ok' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'It should render the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'The response body should contain placeholder text' do
+      expect(response.body).to include('Bio')
     end
   end
 end
