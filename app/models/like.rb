f864cc2eb0fff63a942e64ccaf0b9867aecbbdfe
Kenny <kennysalazar3197@gmail.com>
2022-06-06 20:35:03 -0500

    Add method to Like for updating the likes_counter column for a given post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  scope :update_likes_counter, ->(post, count) { post.update(likes_counter: count) }
 end
