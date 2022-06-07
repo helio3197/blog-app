0e5e339e40eacbc50aef27a1e1b038800ae8ec40
Kenny <kennysalazar3197@gmail.com>
2022-06-06 20:16:38 -0500

    Add method to Post for updating the postos_counter column for a given user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  scope :update_posts_counter, ->(user, count) { user.update(posts_counter: count) }
 end
