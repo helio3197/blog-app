622772a0a581f37e89b4530ff8926b8f5a83d443
Kenny <kennysalazar3197@gmail.com>
2022-06-06 20:33:21 -0500

    Add method to Comment for updating the comments_counter column for a given post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  scope :update_comments_counter, ->(post, count) { post.update(comments_counter: count) }
 end
