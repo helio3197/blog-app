e07286d887e98813d01eb7ca4047be8fe7000ed2
Kenny <kennysalazar3197@gmail.com>
2022-06-09 13:25:20 -0500

    Remove unused commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    # @last_comments = Post.last_five_comments()
   end
 
   def show
