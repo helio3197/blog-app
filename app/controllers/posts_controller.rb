34c8a21843df43b70332b8ffe53e583378d4713b
Kenny <kennysalazar3197@gmail.com>
2022-06-09 12:17:31 -0500

    Update Post#index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    # @last_comments = Post.last_five_comments()
+  end
 
   def show; end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
   scope :update_posts_counter, ->(user, count) { user.update(posts_counter: count) }
   scope :last_five_comments, ->(post) { Comment.where(post:).last(5) }
+
+  def last_five_comments
+    comments.last(5)
+  end
 end
