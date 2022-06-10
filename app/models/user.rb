9ec9fcc56d422e4054acbe75403b6fbc4b2bbfda
Kenny <kennysalazar3197@gmail.com>
2022-06-10 12:39:08 -0500

    Add model validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   scope :last_three_posts, ->(user) { Post.where(author: user).last(3) }
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 end
