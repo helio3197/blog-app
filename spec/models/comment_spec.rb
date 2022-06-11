ef9bc35ca8b0705344e5d9b38012b4dd73596a05
Kenny <kennysalazar3197@gmail.com>
2022-06-10 21:11:14 -0500

    Add unit tests to Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(author: User.first, post: Post.first, text: 'Testing') }
+
+  it 'Should be valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'Should have a #update_comments_counter method' do
+    comments_count = subject.post.comments_counter
+
+    expect do
+      subject.save
+    end
+      .to change { subject.post.comments_counter }.from(comments_count).to(comments_count + 1)
+  end
 end
