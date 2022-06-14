d1ad6d15de00f4bfcdb3784805aaa14ff1d7ba9e
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-15 01:54:04 +0500

    Add delete button and permissions for Post

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,10 +9,10 @@ class Ability
     can :read, :all # start by defining rules for all users, also not logged ones
     return unless user.present?
 
-    can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
-    can :manage, Comment, user_id: user.id
-    can :manage, Like, user_id: user.id
-    return unless user.admin?
+    can :manage, Post, author_id: user.id # if the user is logged in can manage it's own posts
+    can :manage, Comment, author_id: user.id
+    can :manage, Like, author_id: user.id
+    return unless user.role == 'admin'
 
     can :manage, :all # finally we give all remaining permissions only to the admins
 
