c1d40555e905cac56c158f6282c4a04d9e1fa3cc
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-16 21:39:50 +0500

    Add test index page Can see number of posts for each user

--- spec/system/index_page_spec.rb
+++ spec/system/index_page_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe 'Index page', type: :system do
     visit root_path
     expect(page).to have_content('Jhon')
   end
-
+  
   it 'I can see the profile picture for each user.' do
     visit root_path
     expect(page.find('img')['src']).to have_content 'https://via.placeholder.com/150'
   end
 
-  
+  it 'I can see the number of posts each user has written.' do
+    visit root_path
+    expect(page).to have_content('Number of posts: 1')
+  end
+
 end
